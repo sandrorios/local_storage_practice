759e480cf04bd5841dbcf79e9c11ea46d726f35b
sandrorios <sandroderios@hotmail.com>
2023-12-04 16:06:20 -0500

    added the DOM stuff

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let listItem = document.getElementById('list-item');
 let inputField = document.getElementById('input-field');
 let filterItem = document.getElementById('filter')
 let clearBtn = document.getElementById('clear');
+
+function onAddToSubmit(e){
+    e.preventDefault();
+    let newItem = inputField.value;
+    if(newItem === ''){
+        alert("Please add an item");
+        return;
+    }
+
+    addItemToDOM(newItem);
+    addItemToStorage(newItem);
+}
+
+formItem.addEventListener('submit', onAddToSubmit);
